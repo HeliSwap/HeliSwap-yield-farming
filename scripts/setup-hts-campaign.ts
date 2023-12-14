4e1557823c70d330e7c1b0665f6c76fbb82fa0f8
Martin Dobrev <martindobrev87@gmail.com>
2023-12-14 15:41:10 +0200

    Script for mainnet updated

--- scripts/setup-hts-campaign.ts
+++ scripts/setup-hts-campaign.ts
@@ -1,5 +1,6 @@
 // @ts-nocheck
 import hardhat from 'hardhat';
+import axios from 'axios';
 
 const enableRewards = require('./03-enable-rewards');
 const sendRewards = require('./04-send-reward');
@@ -10,13 +11,26 @@ export const addressToId = (tokenAddress: string) => {
   return hethers.utils.asAccountString(tokenAddress);
 };
 
+export const requestIdFromAddress = async (id: string) => {
+  const url = `https://mainnet-public.mirrornode.hedera.com/api/v1/contracts/${id}`;
+  try {
+    const {
+      data: { contract_id },
+    } = await axios(url);
+    return contract_id;
+  } catch (e) {
+    console.error(e);
+    return '0';
+  }
+};
+
 async function setupHTSCampaign(campaign: string, reward: string, amount: string, duration: string) {
   const deployer = (await hardhat.hethers.getSigners())[0];
 
   const deployerId = hardhat.hethers.utils.asAccountString(deployer.address);
   const deployerPk = deployer.identity.privateKey;
 
-  const campaignId = addressToId(campaign);
+  const campaignId = await requestIdFromAddress(campaign);
   const rewardId = addressToId(reward);
 
   //   2. Approve Reward
